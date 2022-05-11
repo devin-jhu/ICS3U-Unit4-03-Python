87a18f91d02ca483851a71c303be1a5cdee43a8b
Devin Jhu <devin.jhu@mths.ca>
2022-05-11 03:17:57 +0000

    code

--- any_number.py
+++ any_number.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 # Created by Devin Jhu
-# Created on March 2022
-# The area and perimeter calculator
+# Created on May 2022
+# The loop multiplier
 
 
 def main():
-    # this program shows the sum of all numbers from 0 to number
+    # this program shows the sum of number entered
     counter = 0
     sum = 0
 
@@ -16,10 +16,12 @@ def main():
     # process & output
     try:
         number_int = int(number)
-        while counter < number_int:
-            sum = sum + (counter + 1)
-            counter = counter + 1
-        print("The sum of all numbers to {0} is {1}".format(number_int, sum))
+        if number_int <= 0:
+            print("Not a positive number")
+        else:
+            for counter in range(number_int + 1):
+                sum = counter * counter
+                print("{0}² = {1}".format(counter, sum))
 
     except Exception:
         print("Not a number.")
